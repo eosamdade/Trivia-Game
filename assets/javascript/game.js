f38613c3bea9db586fd77ecb277fc5718a8a075b
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-15 21:28:35 -0500

    worked on reset,start,time, in the .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  
     var options = [
         {
             question:"",
@@ -32,7 +32,7 @@ $(document).ready(function(){
             photo: ""
         }
     ]
-
+    //my global variables
     var correct = 0;
     var wrong = 0;
     var unaswered = 0;
@@ -45,12 +45,58 @@ $(document).ready(function(){
     var index;
     var newArray = [];
     var holder = [];
-
+    var drumroll = $("myAudio")
+    
+    //reset game
     $("#reset").hide();
-
+    
+    //start game
     $("#start").on("click",function(){
-        $("#start")
-    })
+        $("#start").hide();
+        displayQuestion();
+        runTimer();
+        for (var i=0; i<options.length; i++) {
+            holder.push(option[i]);
+        };
+
+    });
+
+    //time
+    function runTimer () {
+        if (!running) {
+            intervalId = setInterval(decreament,1000)
+        };
+
+        running = true;
+    };
+
+    function decreament () {
+       
+        $("#timeleft").html("<h3>TIME REMAINING IS " + timer + "</h3>");
+        timer --;
+
+        if (timer === 0) {
+            unaswered ++;
+            stop();
+
+            $("answerdblock").html("<p>TIMER IS UP! The correct ans is" + drumroll + "</p>");
+            hidepicture();
+        };
+        
+    };
+
+    function drumroll () {
+        drumroll.play()
+    };
+
+    function stop () {
+        running = false;
+        clearInterval(intervalId)
+    }
+
+
+
+
 
     
 
